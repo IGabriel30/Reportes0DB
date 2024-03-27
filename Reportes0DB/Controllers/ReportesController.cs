8c92a298c43dcfaf45f4be3ec0d6b7b72b4ca451
Ingrid Jacqueline Gabriel Zepeda <ingridgabriel30@hotmail.com>
2024-03-26 22:44:07 -0600

    Controlador Reporte, Vista ReporteOrdenes e Index.

---
+++ Reportes0DB/Controllers/ReportesController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Reportes0DB.Models;
+using Rotativa.AspNetCore;
+
+namespace Reportes0DB.Controllers
+{
+    public class ReportesController : Controller
+    {
+        private readonly Reportes0DBContext _context;
+
+        public ReportesController(Reportes0DBContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> GenerarReporte(string customerID)
+        {
+
+            var ordenes = await _context.Orders
+                .Include(s => s.Customer)
+                .Include(s => s.OrderDetails)
+                .ThenInclude(s => s.Product)
+                .ThenInclude(s => s.Category)
+                .Include(s => s.Employee)
+                .Where(s => s.CustomerId == customerID)
+                .OrderByDescending(s => s.OrderDate)
+                .ToListAsync();
+            return new ViewAsPdf("ReporteOrdenes", ordenes)
+            {
+                //..
+            };
+        }
+    }
+}
